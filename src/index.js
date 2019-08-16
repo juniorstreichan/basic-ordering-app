33c2a8b1f8d7edf25071cea192ccae1904201cf5
Junior Streichan <juniorstreichan.dev@gmail.com>
2019-08-16 09:02:49 -0400

    Dependencies and configs

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { YellowBox } from 'react-native';
 
 import Routes from './routes';
 
 import './config/StatusBar';
 import { Container } from './components';
+
+YellowBox.ignoreWarnings(['UNSAFE_']);
 
 export default function App() {
   return (
